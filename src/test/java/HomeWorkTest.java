fda1e7a98f2232e6917b6adcd10eeeb8699945d6
JulWas <julia.wasilewska.18@gmail.com>
2023-06-24 23:23:55 +0200

    Commit HomeWork1

--- src/test/java/HomeWorkTest.java
+++ src/test/java/HomeWorkTest.java
@@ -2,49 +2,49 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomeWorkTest extends BaseTest{
-    @Test(expectedExceptions = AssertionError.class, priority = 6)
+    @Test(expectedExceptions = AssertionError.class, priority = 6, groups = "Int")
     public void divInt_1(){
         int expectedValue = 50;
         int actualValue = calculator.divInt(0, 100);
         Assert.assertEquals(actualValue, expectedValue, "value is not equals");
     }
-    @Test(priority = 3)
+    @Test(priority = 3, groups = "Int")
     public void divInt_2(){
         int expectedValue = 0;
         int actualValue = calculator.divInt(0, 100);
         Assert.assertEquals(actualValue, expectedValue, "value is not equals");
     }
-@Test(priority = 1)
+@Test(priority = 1, groups = "Int")
     public void divInt_3(){
         int expectedValue = 50;
         int actualValue = calculator.divInt(100, 2);
         Assert.assertEquals(actualValue, expectedValue, "value is not equals");
     }
-    @Test(priority = 8, description = "checked division by zero")
+    @Test(priority = 8, description = "checked division by zero", groups = "Int")
     public void divInt_4() {
         int expectedValue = 0;
         int actualValue = calculator.divInt(100, 0);
         Assert.assertEquals(actualValue, expectedValue);
     }
- @Test(priority = 4)
+ @Test(priority = 4, groups = "Double")
     public void divDouble_1(){
         int expectedValue = 0;
         double actualValue = calculator.divDouble(0,10.7);
         Assert.assertEquals(actualValue, expectedValue, "value is not equals");
     }
-@Test(expectedExceptions = AssertionError.class, priority = 5)
+@Test(expectedExceptions = AssertionError.class, priority = 5, groups = "Double")
     public void divDouble_2(){
         int expectedValue = 15;
         double actualValue = calculator.divDouble(60.2, 10.7);
         Assert.assertEquals(actualValue, expectedValue, "value is not equals");
     }
-    @Test(priority = 2)
+    @Test(priority = 2, groups = "Double")
     public void divDouble_3(){
         int expectedValue = 5;
         double actualValue = calculator.divDouble(60.2, 10.7);
         Assert.assertEquals(actualValue, expectedValue, "value is not equals");
     }
-@Test(enabled = false, priority = 7)
+@Test(enabled = false, priority = 7, groups = "Double")
     public void divDouble_4(){
         int expectedValue = 0;
         double actualValue = calculator.divDouble(100.5, 0);
