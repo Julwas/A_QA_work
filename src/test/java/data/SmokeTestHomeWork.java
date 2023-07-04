de1777202b441b087bf5943849d6d45193b09527
JulWas <julia.wasilewska.18@gmail.com>
2023-07-04 09:08:26 +0200

    retry and listener

--- src/test/java/data/SmokeTestHomeWork.java
+++ src/test/java/data/SmokeTestHomeWork.java
@@ -14,6 +14,7 @@ import static java.lang.Thread.sleep;
 
 public class SmokeTestHomeWork {
     protected WebDriver driver;
+
     @BeforeMethod
     public void setUp() {
         BrowserFactory browserFactory = new BrowserFactory();
@@ -21,40 +22,40 @@ public class SmokeTestHomeWork {
     }
 
     @Test
-    public  void calcElectricFloor() throws InterruptedException {
+    public void calcElectricFloor() throws InterruptedException {
         driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
-            WebElement wightInput = driver.findElement(By.id("el_f_width"));
-            WebElement lengthInput = driver.findElement(By.id("el_f_lenght"));
-            wightInput.sendKeys("3");
-            lengthInput.sendKeys("4");
+        WebElement wightInput = driver.findElement(By.id("el_f_width"));
+        WebElement lengthInput = driver.findElement(By.id("el_f_lenght"));
+        wightInput.sendKeys("3");
+        lengthInput.sendKeys("4");
 
-            WebElement selectWebElementRoomType = driver.findElement(By.id("room_type"));
-            Select selectRoomType = new Select(selectWebElementRoomType);
-            selectRoomType.selectByVisibleText("Ванная");
-            sleep(1000);
+        WebElement selectWebElementRoomType = driver.findElement(By.id("room_type"));
+        Select selectRoomType = new Select(selectWebElementRoomType);
+        selectRoomType.selectByVisibleText("Ванная");
+        sleep(1000);
 
-            WebElement selectWebElementHeatingType = driver.findElement(By.id("heating_type"));
-            Select selectHeatingType = new Select(selectWebElementHeatingType);
-            selectHeatingType.selectByVisibleText("Основное отопление");
-            sleep(1000);
+        WebElement selectWebElementHeatingType = driver.findElement(By.id("heating_type"));
+        Select selectHeatingType = new Select(selectWebElementHeatingType);
+        selectHeatingType.selectByVisibleText("Основное отопление");
+        sleep(1000);
 
-            WebElement elFlossesInput = driver.findElement(By.id("el_f_losses"));
-            elFlossesInput.sendKeys("300");
+        WebElement elFlossesInput = driver.findElement(By.id("el_f_losses"));
+        elFlossesInput.sendKeys("300");
 
-            WebElement calcButton = driver.findElement(By.name("button"));
-            calcButton.click();
-            sleep(1000);
+        WebElement calcButton = driver.findElement(By.name("button"));
+        calcButton.click();
+        sleep(1000);
 
-            WebElement resultFloorCablePower = driver.findElement(By.id("floor_cable_power"));
-            Assert.assertEquals(resultFloorCablePower.getAttribute("value"), "315");
+        WebElement resultFloorCablePower = driver.findElement(By.id("floor_cable_power"));
+        Assert.assertEquals(resultFloorCablePower.getAttribute("value"), "315");
 
-            WebElement resultSpecFloorCablePower = driver.findElement(By.id("spec_floor_cable_power"));
-            Assert.assertEquals(resultSpecFloorCablePower.getAttribute("value"), "26");
+        WebElement resultSpecFloorCablePower = driver.findElement(By.id("spec_floor_cable_power"));
+        Assert.assertEquals(resultSpecFloorCablePower.getAttribute("value"), "26");
+    }
 
-}
     @AfterMethod
 
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
