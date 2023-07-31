323f88d1f3dc5a3cda2f0b8127084f7da2467fcd
JulWas <julia.wasilewska.18@gmail.com>
2023-07-31 17:48:57 +0200

    maven cw

---
+++ src/test/java/testes/AllureTest.java
@@ -0,0 +1,17 @@
+package testes;
+
+import jdk.jfr.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AllureTest {
+    @Test
+    @Description("Some detailed test description. ")
+    public void TestName1(){
+        Assert.assertTrue(true);
+    }
+    @Test(description = "Human read")
+    public void TestName2(){
+        Assert.assertTrue(true);
+    }
+}
