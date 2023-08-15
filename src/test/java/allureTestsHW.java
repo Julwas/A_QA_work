b7ff3248236b66f16896b5362dca2a29a8b54b05
JulWas <julia.wasilewska.18@gmail.com>
2023-08-15 17:17:27 +0200

    hw push

--- src/test/java/allureTestsHW.java
+++ src/test/java/allureTestsHW.java
@@ -1,4 +1,7 @@
 import baseEntities.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CartPage;
@@ -7,10 +10,12 @@ import pages.CheckoutOverviewPage;
 import pages.ProductsPage;
 import utils.configuration.ReadProperties;
 
-public class E2ETest extends BaseTest {
+public class allureTestsHW extends BaseTest {
 
-    @Test
-    public void E2ETest() {
+   @Test(description = "End to end test by product.")
+   @Description("Verifies the entire process of purchasing an item on the website.")
+   @Severity(SeverityLevel.CRITICAL)
+    public void Test1() {
         Assert.assertTrue(loginStep.successLogin(ReadProperties.username(), ReadProperties.password()
         ).isPageOpened());
         Assert.assertTrue(productsStep.AddProducts().isPageOpened());
@@ -27,5 +32,19 @@ public class E2ETest extends BaseTest {
         CheckoutCompletePage checkoutCompletePage =new CheckoutCompletePage(driver);
         checkoutCompletePage.getBackHomeButton().click();
         Assert.assertTrue(new ProductsPage(driver).isPageOpened());
+    }
+    @Test(description = "Successful login test.")
+    @Description("Test with correct login and correct password.")
+    @Severity(SeverityLevel.MINOR)
+    public void Test2() {
+        Assert.assertTrue(
+                loginStep.successLogin("standard_user", ReadProperties.password()).isPageOpened());
+    }
+    @Test(description = "Not successful login test.")
+    @Description("Test with wrong login and correct password.")
+    @Severity(SeverityLevel.NORMAL)
+    public void Test3() {
+        Assert.assertTrue(
+                loginStep.successLogin("standarb_user", ReadProperties.password()).isPageOpened());
     }
 }
