b3e679da81fa742b6a2a7bf0aa8017f8eb65e9cd
JulWas <julia.wasilewska.18@gmail.com>
2023-09-01 13:16:01 +0200

    modern

--- src/main/java/factory/BrowserFactory.java
+++ src/main/java/factory/BrowserFactory.java
@@ -22,8 +22,7 @@ public class BrowserFactory {
         switch (ReadProperties.browserName().toLowerCase()) {
             case "chrome" :
                 driverManagerType = DriverManagerType.CHROME;
-                WebDriverManager.getInstance(driverManagerType).setup();
-                //WebDriverManager.chromedriver().driverVersion("114.0.5735.90").setup();
+                WebDriverManager.getInstance(driverManagerType).clearDriverCache().setup();
 
                 driver = new ChromeDriver(getChromeOptions());
                 break;
